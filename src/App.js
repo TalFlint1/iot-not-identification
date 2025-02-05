e2d9b92fdc77faa8b0a2ae123fbf09016977fc40
TalFlint1 <tal.flint@live.biu.ac.il>
2025-02-05 20:55:47 +0200

    finished register screen

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import SettingsScreen from "./components/SettingsScreen";
 import InputScreen from "./components/InputScreen";
 import LoginScreen from "./components/LoginScreen";
+import RegisterScreen from "./components/RegisterScreen";
 
 function App() {
-  return <LoginScreen />;
+  return <RegisterScreen />;
 }
 
 export default App;
