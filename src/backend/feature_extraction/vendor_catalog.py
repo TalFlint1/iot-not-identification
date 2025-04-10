3bbbeab2ad424644285f562a92a0ba8c37b67bc4
TalFlint1 <tal.flint@live.biu.ac.il>
2025-04-10 12:24:43 +0300

    removing not relevant vendors

--- src/backend/feature_extraction/vendor_catalog.py
+++ src/backend/feature_extraction/vendor_catalog.py
@@ -1,14 +1,14 @@
-vendors = ['siemens', 'ptc', 'abb', 'intel', 'schneider electric', 'honeywell', 'huawei', 'sap', 'advantech', 'bosch', 'cisco'
+vendors = ['siemens', 'ptc', 'intel', 'schneider electric', 'honeywell', 'huawei', 'sap', 'bosch', 'cisco'
            , 'ibm', 'texas instruments', 'c3 iot', 'nxp semiconductors', 'microsoft', 'micron technology', 
            'ericsson', 'samsung', 'digi', 'rockwell automation', 'broadcom', 'telefonica', 
-           'mitsubishi', 'dell', 'nvidia', 'qualcomm', 'analog devices', 'ni', 'general electric', 'infineon', 
-           'at&t', 'stmicroelectronics', 'emerson', 'airtel', 'relayr', 'vodafone', '2lemetry', 'phoenix contact', 
-           '3m company', 'arm', 'google', 'eaton', 'accenture', 'omron industrial automation', 'ayla networks', 'juniper networks', 
+           'mitsubishi', 'dell', 'nvidia', 'qualcomm', 'ni', 'general electric', 'infineon', 
+            'stmicroelectronics', 'emerson', 'relayr', 'vodafone', 'phoenix contact', 'google', 'eaton', 
+           'omron industrial automation', 'ayla networks', 'juniper networks', 
            'nokia', 'hitachi', 'silicon labs', 'dxc technology', 'john deere', 'infor', 'microchip technology', 'hewlett packard', 
            'hpe', 'hewlett-packard', 'telit', 'silver spring networks', 'cognizant', 'lantronix', 'vantiq', 'dji enterprise', 'sparkcognition', 'zscaler', 
            'wibu-systems', 'particle', 'on semiconductor', 'nec', 'caterpillar inc.', 'impinj', 'johnson controls', 'inductive automation', 
-           'ingenu', 'panasonic', 'auvesy-mdt', 'toshiba', 'raytheon technologies', 'amd', 'tesla', 'autodesk', 'cree', 'telstra', 
+           'ingenu', 'panasonic', 'toshiba', 'raytheon technologies', 'tesla', 'cree', 'telstra', 
            'deutsche telekom', 'te connectivity', 'tyco electronics', 'eseye', 'osisoft', 'china mobile', 'zebra technologies', 'moxa', 'proglove', 'axoom', 
-           'trumpf', 'worldsensing', 'microsemi corporation', 'sigfox', 'cypress semiconductor', 'alibaba cloud', 'samsara', 'sierra wireless', 
+           'trumpf', 'worldsensing', 'microsemi corporation', 'sigfox', 'cypress semiconductor', 'samsara', 'sierra wireless', 
            'pulse electronics', 'gemalto', 'edimax', 'piper', 'amazon', 'smartthings', 'insteon', 'xiaomi', 'roku', 'belkin', 'nest',
             'dlink', 'd-link', 'caseta', 'philips', 'apple', 'canary', 'wansview', 'wyze', 'netatmo', 'hp', 'irobot', 'blink', 'sonos', 'netgear', 'fitbit' ]
