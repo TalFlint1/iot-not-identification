98ad35e21f3776f9052a168d0c89476934a92b81
TalFlint1 <tal.flint@live.biu.ac.il>
2025-04-11 14:15:36 +0300

    save changes of vendors

--- src/backend/feature_extraction/vendor_catalog.py
+++ src/backend/feature_extraction/vendor_catalog.py
@@ -1,14 +1,14 @@
-vendors = ['siemens', 'ptc', 'schneider electric', 'honeywell', 'huawei', 'sap', 'bosch', 'cisco'
-           , 'texas instruments', 'nxp semiconductors', 'microsoft',
+vendors = ['siemens', 'schneider electric', 'honeywell', 'huawei', 'sap', 'bosch', 'cisco'
+           , 'texas instruments',
            'samsung', 'digi', 'rockwell automation', 'telefonica', 
-           'nvidia', 'qualcomm', 'ni', 'infineon', 
-            'stmicroelectronics', 'relayr', 'vodafone', 'phoenix contact', 'google', 'eaton', 
+           'nvidia', 'qualcomm', 'infineon', 
+            'stmicroelectronics', 'relayr', 'vodafone', 'google', 'eaton', 
            'omron industrial automation',
            'nokia', 'silicon labs',
         'telit', 'silver spring networks', 'vantiq', 'dji enterprise', 'sparkcognition', 'zscaler', 
-           'wibu-systems', 'particle', 'on semiconductor', 'nec', 'impinj', 'johnson controls', 
-            'panasonic', 'toshiba', 'raytheon technologies', 'tesla', 'telstra', 'xiaoyi',
-            'te connectivity', 'tyco electronics', 'eseye', 'osisoft', 'zebra technologies', 'moxa', 'proglove',
+           'wibu-systems', 'impinj', 'johnson controls', 
+            'panasonic', 'toshiba', 'tesla', 'telstra', 'xiaoyi',
+            'te connectivity', 'tyco electronics', 'eseye', 'zebra technologies', 'moxa', 'proglove',
            'trumpf', 'worldsensing', 'sigfox', 'cypress semiconductor', 'samsara', 'sierra wireless', 
-           'pulse electronics', 'edimax', 'piper', 'amazon', 'smartthings', 'insteon', 'xiaomi', 'roku', 'belkin', 'nest',
+            'edimax', 'piper', 'amazon', 'smartthings', 'insteon', 'xiaomi', 'roku', 'belkin', 'nest',
             'dlink', 'd-link', 'caseta', 'philips', 'apple', 'canary', 'wansview', 'wyze', 'netatmo', 'hp', 'irobot', 'blink', 'sonos', 'netgear', 'fitbit' ]
