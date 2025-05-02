e344e5b5c705ddf763ed27cf744c61c3e04db171
TalFlint1 <tal.flint@live.biu.ac.il>
2025-05-02 15:55:06 +0300

    set firebase for google login

---
+++ src/frontend/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDr-FwDJRbtj_x_99XB9SB6YSAzqUGTRxE",
+  authDomain: "iotproject-66dce.firebaseapp.com",
+  projectId: "iotproject-66dce",
+  storageBucket: "iotproject-66dce.firebasestorage.app",
+  messagingSenderId: "855765079214",
+  appId: "1:855765079214:web:b1cc409731b47763e5493f",
+  measurementId: "G-9TX4Y8FV77"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app }; // export the app for other parts of your app to use
